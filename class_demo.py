a9da147e779b860acc7d48850bb580a726cdb585
roshanpandey <11rshan@gmail.com>
2025-08-31 14:02:13 +0545

    Added new files

--- class_demo.py
+++ class_demo.py
@@ -1,23 +1,73 @@
-from python_demo import student
+# student_name = "Sandesh"
+# student_age = 18
+#
+# def display_info(student_name, student_age):
+#     print(f"Student Name: {student_name}, Student Age: {student_age}")
+#
+# display_info(student_name, student_age)
+#
+
+class Student:
+    def __init__(self):
+        self.name = "Ram"
+        self.subject = "Python"
+
+    def display_info(self):
+        print(f"Student Name: {self.name}, Student Class: {self.subject}")
 
 
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def describe(self):
-        print(f"Parent: {self.name} is {self.age} years old.")
+student1 = Student()
+student1.display_info()
 
 
-class Student(Person):
-    def __init__(self, name, age, marks):
-        super().__init__(name, age)
-        self.marks = marks
+#
+#
+#
+# class PythonStudent( Student):
+#     def __init__(self, name, age):
+#         super().__init__(name, age)
+#         self.__course = "Python"
+#
+#     def display_course(self):
+#         print(self.__course)
+    #
+    # def display_info(self):
+    #     print(f"Python Student Name: {self.name}, Student Age: {self.age}")
+#
+# python_student = PythonStudent("Dev Raj", 35)
+# python_student.__course = "Java"
+# python_student.display_course()
 
-    def describe(self):
+"""
+private int number = 10
+"""
 
-        print(f"Student: {self.name} is {self.age} years old.")
 
-student = Student("Ram", 25, "Reading")
-student.describe()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+function
+
+student= [{
+"name": "Ram",
+"age": 20
+},
+{
+"name": "Hari",
+"age": 20
+}]
+student.display_info()
+
+"""
